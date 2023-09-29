e03bb9511839b0970b17e19d62a77b6a3c884fc6
kwamboka3 <pkwamboka@flag42.com>
2023-09-29 14:45:39 +0300

    100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,27 +1,9 @@
 export default function createIteratorObject(report) {
-  const departments = Object.keys(report);
-
-  let currentDeptIndex = 0;
-  let currentEmployeeIndex = 0;
-
-  return {
-    next() {
-      if (currentDeptIndex < departments.length) {
-        const departmentName = departments[currentDeptIndex];
-        const employees = report[departmentName];
-
-        if (currentEmployeeIndex < employees.length) {
-          const employee = employees[currentEmployeeIndex];
-          currentEmployeeIndex++;
-          return { value: employee, done: false };
-        } else {
-          currentDeptIndex++;
-          currentEmployeeIndex = 0;
-          return this.next();
-        }
+  return (function* _() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
       }
-
-      return { done: true };
-    },
-  };
+    }
+  }());
 }
