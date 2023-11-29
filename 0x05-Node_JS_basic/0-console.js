d96006720de05308069026fdf582a3e5688ba7b9
kwamboka3 <pkwamboka@flag42.com>
2023-11-29 19:26:43 +0300

    0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,5 @@
+// import the displayMessage function from the 0-console.js file
+const { displayMessage } = require('./0-console');
+
+// call the function to display a message
+displayMessage('Hello, world!');
