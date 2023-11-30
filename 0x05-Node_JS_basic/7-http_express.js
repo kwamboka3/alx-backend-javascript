53afb3d8a8bf56cef49ba37246af5b5de048829a
kwamboka3 <pkwamboka@flag42.com>
2023-11-30 19:56:31 +0300

    7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,12 +1,5 @@
 const fetch = require("node-fetch");
-
-const url = "http://localhost:8000/data";
-
+const url = "http://localhost:8080/plates.json";
 fetch(url).then(response => {
     response.json().then(json => {
         console.log(json);
-    });
-})
-.catch(error => {
-    console.log(error);
-});
